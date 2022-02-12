6d03605867676600734b347eeb9a26dd22d1a774
Aayush <aayushdhand@gmail.com>
2022-02-13 01:08:18 +0530

    code rolled back to paytm testing

--- lib/config_paytm.php
+++ lib/config_paytm.php
@@ -7,10 +7,10 @@
 - Above details will be different for testing and production environment.
 */
 
-define('PAYTM_ENVIRONMENT', 'PROD'); // PROD
-define('PAYTM_MERCHANT_KEY', 'bGysLqxsrs1%K_Gi'); //Change this constant's value with Merchant key received from Paytm.
-define('PAYTM_MERCHANT_MID', 'RNHVVU67156141772513'); //Change this constant's value with MID (Merchant ID) received from Paytm.
-define('PAYTM_MERCHANT_WEBSITE', 'DEFAULT'); //Change this constant's value with Website name received from Paytm.
+define('PAYTM_ENVIRONMENT', 'TEST'); // PROD
+define('PAYTM_MERCHANT_KEY', 'Dc_c3tADsFaqLsEW'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', 'vLQkAT72546842461929'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_WEBSITE', 'WEBSTAGING'); //Change this constant's value with Website name received from Paytm.
 
 $PAYTM_STATUS_QUERY_NEW_URL='https://securegw-stage.paytm.in/merchant-status/getTxnStatus';
 $PAYTM_TXN_URL='https://securegw-stage.paytm.in/theia/processTransaction';
