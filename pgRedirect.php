3264a12f2a2eed53d9246567b9992865059f627a
Aayush <aayushdhand@gmail.com>
2022-02-13 01:37:53 +0530

    calling the paytm fetch api

--- pgRedirect.php
+++ pgRedirect.php
@@ -15,8 +15,7 @@ $CUST_ID = $_POST["custID"];
 $INDUSTRY_TYPE_ID = $_POST["INDUSTRY_TYPE_ID"];
 $CHANNEL_ID = $_POST["CHANNEL_ID"];
 $TXN_AMOUNT = $_POST["amount"];
-$PAYTM_FETCH_USER_BALANCE = "https://securegw-stage.paytm.in/userAsset/fetchBalanceInfo?mid='".PAYTM_MERCHANT_MID."'&orderId='".ORDER_ID."'";
-$FETCH_PAYMENT_OPTION = "https://securegw-stage.paytm.in/theia/api/v2/fetchPaymentOptions?mid='".PAYTM_MERCHANT_MID."'&orderId='".ORDER_ID."'";
+
 // Create an array having all required parameters for creating checksum.
 $paramList["MID"] = PAYTM_MERCHANT_MID;
 $paramList["ORDER_ID"] = $ORDER_ID;
@@ -45,7 +44,7 @@ $checkSum = getChecksumFromArray($paramList,PAYTM_MERCHANT_KEY);
 </head>
 <body>
 	<center><h1>Please do not refresh this page...</h1></center>
-		<form method="post" action="<?php echo $FETCH_PAYMENT_OPTION ?>" name="f1">
+		<form method="post" action="<?php echo PAYTM_TXN_URL ?>" name="f1">
 		<table border="1">
 			<tbody>
 			<?php
